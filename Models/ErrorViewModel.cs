27a4a9965c5404a19bde14f584a96498037b3bca
W0793709 <W0793709@ADL-A1201H-19>
2023-10-13 13:26:04 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcMagnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
