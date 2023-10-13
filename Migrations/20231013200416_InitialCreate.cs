ba0571d56046f5c1fa5747f17abe2c162a4ee3e6
W0793709 <W0793709@ADL-A1201H-19>
2023-10-13 16:10:32 -0400

    Initial migration

---
+++ Migrations/20231013200416_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMagnet.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Magnet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Life = table.Column<string>(nullable: true),
+                    type = table.Column<string>(nullable: true),
+                    color = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Magnet", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Magnet");
+        }
+    }
+}
